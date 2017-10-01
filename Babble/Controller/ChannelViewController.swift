e233d02beae20112fcdbe1171cf59094279c1e1d
Pankaj Kumar Jha <pankaj@aleph-labs.com>
2017-10-01 13:06:50 +0800

    Added SWReveal

---
+++ Babble/Controller/ChannelViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ChannelViewController.swift
+//  Babble
+//
+//  Created by Pankaj on 01/10/17.
+//  Copyright © 2017 Aleph-Labs. All rights reserved.
+//
+
+import UIKit
+
+class ChannelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width  - 60
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
---
+++ Babble/Controller/ChatViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ChatViewController.swift
+//  Babble
+//
+//  Created by Pankaj on 01/10/17.
+//  Copyright © 2017 Aleph-Labs. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+    }
+}
